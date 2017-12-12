59bcdcd2c32cd8d1147eea4117b1fd91024cc8c1
urushiyama <aswif10flis1ntkb@gmail.com>
2017-12-12 21:00:58 +0900

    light関連の字句解析を追加し、関連字句ごとにwhen文を並び替えた

--- lexer.rb
+++ lexer.rb
@@ -1,6 +1,5 @@
 class Lexer
   def initialize(text)
-    # @remain = text.gsub(/(\r\n|\r|\n)/, " \n")
     @lines = text.lines
     @remain = @lines.shift
     @line_number = 1
@@ -21,230 +20,249 @@ class Lexer
           @line_number += 1
         end
       end
-      # index = @remain.index("\n")
-      # if index.nil?
-      #   remain_top = @remain
-      #   remain_last = nil
-      # else
-      #   remain_top = @remain[0, index+1]
-      #   remain_last = @remain[index+1, @remain.length]
-      # end
-      # case remain_top
       case @remain
-      when /^object/
-        @token = :object
-        @match = $&
-        @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
-        yield @token, @match, @remain, @line_number
-        break
-      when /^#declare/
-        @token = :declare
-        @match = $&
-        @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
-        yield @token, @match, @remain, @line_number
-        break
+      # --- Definitions ---
       when /^#version/
         @token = :version
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
       when /^#default/
         @token = :default
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
+        yield @token, @match, @remain, @line_number
+        break
+      when /^#declare/
+        @token = :declare
+        @match = $&
+        @remain = $'
         yield @token, @match, @remain, @line_number
         break
       when /^=/
         @token = :equal
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
-      when /^matrix/
-        @token = :matrix
+      # --- Mesh data ---
+      when /^object/
+        @token = :object
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
       when /^mesh2/
         @token = :mesh
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
       when /^vertex_vectors/
         @token = :vertex
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
       when /^face_indices/
         @token = :face
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
+      # --- Light settings ---
+      when /^light_source/
+        @token = :light_source
+        @match = $&
+        @remain = $'
+        yield @token, @match, @remain, @line_number
+        break
+      when /^color/
+        @token = :color
+        @match = $&
+        @remain = $'
+        yield @token, @match, @remain, @line_number
+        break
+      when /^srgb/
+        @token = :srgb
+        @match = $&
+        @remain = $'
+        yield @token, @match, @remain, @line_number
+        break
+      when /^fade_distance/
+        @token = :fade_distance
+        @match = $&
+        @remain = $'
+        yield @token, @match, @remain, @line_number
+        break
+      when /^fade_power/
+        @token = :fade_power
+        @match = $&
+        @remain = $'
+        yield @token, @match, @remain, @line_number
+        break
+      when /^parallel/
+        @token = :parallel
+        @match = $&
+        @remain = $'
+        yield @token, @match, @remain, @line_number
+        break
+      when /^shadowless/
+        @token = :shadowless
+        @match = $&
+        @remain = $'
+        yield @token, @match, @remain, @line_number
+        break
+      when /^point_at/
+        @token = :point_at
+        @match = $&
+        @remain = $'
+        yield @token, @match, @remain, @line_number
+        break
+      # --- Camera settings ---
       when /^camera/
         @token = :camera
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
       when /^location/
         @token = :location
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
       when /^right/
         @token = :right
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
       when /^up/
         @token = :up
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
       when /^angle/
         @token = :angle
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
       when /^look_at/
         @token = :look_at
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
+      #  transform
       when /^rotate/
         @token = :rotate
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
       when /^translate/
         @token = :translate
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
+      when /^matrix/
+        @token = :matrix
+        @match = $&
+        @remain = $'
+        yield @token, @match, @remain, @line_number
+        break
+      # --- Dycks ---
       when /^\{/
         @token = :lbrace
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
       when /^\}/
         @token = :rbrace
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
       when /^</
         @token = :langle
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
       when /^>/
         @token = :rangle
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
       when /^\(/
         @token = :lparen
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
       when /^\)/
         @token = :rparen
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
-      when /^,/
-        @token = :comma
-        @match = $&
-        @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
-        yield @token, @match, @remain, @line_number
-        break
-      when /^;/
-        @token = :semicolon
-        @match = $&
-        @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
-        yield @token, @match, @remain, @line_number
-        break
+      # --- Numbers ---
       when /^-?[0-9]+\.[0-9]+/
         @token = :real
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
       when /^-?[0-9]+/
         @token = :int
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
+      # --- Comments ---
       when /^\/\/[^\n]*/
         @token = :comment
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
+      # --- Identifiers ---
       when /^[A-Za-z_][A-Za-z0-9_]*/
         @token = :identifier
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
+        yield @token, @match, @remain, @line_number
+        break
+      # --- Separators ---
+      when /^,/
+        @token = :comma
+        @match = $&
+        @remain = $'
+        yield @token, @match, @remain, @line_number
+        break
+      when /^;/
+        @token = :semicolon
+        @match = $&
+        @remain = $'
         yield @token, @match, @remain, @line_number
         break
       when /^\s+/
         @token = :space
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         next
+      # --- Others ---
       when /^\S+/
         @token = :other
         @match = $&
         @remain = $'
-        # @remain << "\n" << remain_last unless remain_last.nil?
         yield @token, @match, @remain, @line_number
         break
       end
